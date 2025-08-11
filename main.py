c70d58f57104f3b950b1b3f0aec63ef0f5d65971
Mauranne <mauranne.lagneau@gmail.com>
2025-08-11 12:22:56 +0200

    change lat and lon attributes into latitude and longitude

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from sqlmodel import Field, Session, SQLModel, create_engine, select
 # Models
 class PeakBase(SQLModel):
     name: str = Field(index=True)
-    lat: Annotated[float, Field(ge=-90, le=90)]
-    lon: Annotated[float, Field(ge=-180, le=180)]
+    latitude: Annotated[float, Field(ge=-90, le=90, index=True)]
+    longitude: Annotated[float, Field(ge=-180, le=180, index=True)]
     altitude: Annotated[int, Field(ge=0)]
 
 class Peak(PeakBase, table=True):
@@ -41,14 +41,14 @@ def create_db_and_tables():
 
 
 def create_initial_peaks():
-    peak_1 = Peak(name="Mount Desert", lat=44.342827, lon=-68.307138, altitude=55)
-    peak_2 = Peak(name="Mount Cadillac", lat=44.409286, lon=-68.247501, altitude=466)
+    peak_1 = Peak(name="Mount Desert", latitude=44.342827, longitude=-68.307138, altitude=55)
+    peak_2 = Peak(name="Mount Cadillac", latitude=44.409286, longitude=-68.247501, altitude=466)
     peak_3 = Peak(
-        name="Mount Taranaki Maunga", lat=-39.296770, lon=174.063399, altitude=2518
+        name="Mount Taranaki Maunga", latitude=-39.296770, longitude=174.063399, altitude=2518
     )
-    peak_4 = Peak(name="Mount Tongariro", lat=-39.1333, lon=175.6500, altitude=1978)
-    peak_5 = Peak(name="Monte Perdido", lat=42.6499974, lon=0.0499998, altitude=3355)
-    peak_6 = Peak(name="Mont Blanc", lat=45.833641, lon=6.864594, altitude=4806)
+    peak_4 = Peak(name="Mount Tongariro", latitude=-39.1333, longitude=175.6500, altitude=1978)
+    peak_5 = Peak(name="Monte Perdido", latitude=42.6499974, longitude=0.0499998, altitude=3355)
+    peak_6 = Peak(name="Mont Blanc", latitude=45.833641, longitude=6.864594, altitude=4806)
 
     with Session(engine) as session:
         session.add(peak_1)
